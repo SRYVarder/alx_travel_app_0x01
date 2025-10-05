ce2b578b6940ccb99e182a86cf46a96452973578
SRYVarder <smallreezy3@gmail.com>
2025-10-05 19:10:25 +0300

    Update models.py

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -1,3 +1,27 @@
+# listings/models.py
 from django.db import models
+from django.contrib.auth.models import User
 
+class Listing(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    location = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Booking(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Booking for {self.listing.title} by {self.user.username}"
 # Create your models here.
+
